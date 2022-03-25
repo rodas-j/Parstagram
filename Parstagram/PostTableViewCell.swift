f88e6875c2c32df3b674fd329a2979489c1fee65
rodas-j <se.rodas.workicho@gmail.com>
2022-03-25 18:21:08 -0400

    Initial Commit

---
+++ Parstagram/PostTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostTableViewCell.swift
+//  Parstagram
+//
+//  Created by Rodas Jateno on 25/03/2022.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
